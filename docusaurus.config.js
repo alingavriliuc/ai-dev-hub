264b7cd06866823e7e3b53d39d957f02316c778e
Alin <alingavriliuc@hotmail.com>
2023-12-19 09:49:26 +0100

    ✨ Update Docusaurus configuration and add Google Analytics plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,68 +1,78 @@
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'AI-DevHub',
-  tagline: 'Your ultimate resource hub for all things Artificial Intelligence and Machine Learning',
-  favicon: 'img/favicon.ico',
-  url: 'https://ai-dev-hub.com/',
-  baseUrl: '/',
-  organizationName: 'AI-DevHub',
-  projectName: 'AI-DevHub',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	title: "AI-DevHub",
+	tagline: "Your ultimate resource hub for all things Artificial Intelligence and Machine Learning",
+	favicon: "img/favicon.ico",
+	url: "https://ai-dev-hub.com/",
+	baseUrl: "/",
+	organizationName: "AI-DevHub",
+	projectName: "AI-DevHub",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-        },
-        blog: {
-          showReadingTime: true,
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
+	plugins: [
+		[
+			"@docusaurus/plugin-google-gtag",
+			{
+				trackingID: "G-V7X4VHX2VL",
+				anonymizeIP: true,
+			},
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      image: 'img/social-card.jpg',
-      navbar: {
-        title: 'AI-DevHub',
-        logo: {
-          alt: 'AI-DevHub Logo',
-          src: 'img/logo.png',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'docSidebar',
-            position: 'left',
-            label: 'Resources',
-          },
-          {
-            href: 'https://github.com/alingavriliuc/ai-dev-hub',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+	presets: [
+		[
+			"classic",
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: "./sidebars.js",
+				},
+				blog: {
+					showReadingTime: true,
+				},
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+			}),
+		],
+	],
+
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			image: "img/social-card.jpg",
+			navbar: {
+				title: "AI-DevHub",
+				logo: {
+					alt: "AI-DevHub Logo",
+					src: "img/logo.png",
+				},
+				items: [
+					{
+						type: "docSidebar",
+						sidebarId: "docSidebar",
+						position: "left",
+						label: "Resources",
+					},
+					{
+						href: "https://github.com/alingavriliuc/ai-dev-hub",
+						label: "GitHub",
+						position: "right",
+					},
+				],
+			},
+			prism: {
+				theme: prismThemes.github,
+				darkTheme: prismThemes.dracula,
+			},
+		}),
 };
 
 export default config;
